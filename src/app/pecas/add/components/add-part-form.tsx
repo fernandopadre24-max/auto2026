63c6165f9baaab457abf9cf135f41b75e1544fbf
fernando sena <fernandopadre24@gmail.com>
2025-10-23 21:12:14 +0000

    Try fixing this error: `Runtime Error: Error: Maximum update depth excee

--- src/app/pecas/add/components/add-part-form.tsx
+++ src/app/pecas/add/components/add-part-form.tsx
@@ -22,7 +22,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
-import { toast } from '@/hooks/use-toast';
+import { useToast } from '@/hooks/use-toast';
 import {
   generateDescriptionAction,
   suggestPriceAction,
