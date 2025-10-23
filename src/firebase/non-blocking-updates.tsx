6878377def0b281e24e11d95df2010fd421a1c05
fernando sena <fernandopadre24@gmail.com>
2025-10-23 21:49:36 +0000

    fix erro

--- src/firebase/non-blocking-updates.tsx
+++ src/firebase/non-blocking-updates.tsx
@@ -16,7 +16,7 @@ import {FirestorePermissionError} from '@/firebase/errors';
  * Initiates a setDoc operation for a document reference.
  * Does NOT await the write operation internally.
  */
-export function setDocumentNonBlocking(docRef: DocumentReference, data: any, options: SetOptions) {
+export function setDocumentNonBlocking(docRef: DocumentReference, data: any, options: SetOptions = { merge: true }) {
   setDoc(docRef, data, options).catch(error => {
     errorEmitter.emit(
       'permission-error',
